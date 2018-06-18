daab36bb2c42d44cd1445d8f8a1f9588de759621
nsssmdghr <39874463+nsssmdghr@users.noreply.github.com>
2018-06-18 19:59:07 +0300

    Create deciles.py

---
+++ Scripts/deciles.py
@@ -0,0 +1,19 @@
+def get_note(valeur, nom_indic, tab_deciles):
+	if nom_indic == 'accessibilite vp':
+		row = 0
+	elif nom_indic == 'accessibilite tc':
+		row = 1
+	elif nom_indic == 'points interet':
+		row = 2
+	elif nom_indic == 'couverture commerciale':
+		row = 3
+	elif nom_indic == 'potentiel commercial':
+		row = 4
+	deciles = tab_deciles[row]
+	i = 0
+	note = 0
+	while valeur >= deciles[i]:
+		note += 1
+		i += 1
+
+	return note

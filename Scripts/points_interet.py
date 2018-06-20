ed473ec6bd91207d87721476604e213f457d9e6e
Chifoumi78 <39900864+Chifoumi78@users.noreply.github.com>
2018-06-20 18:57:00 +0200

    Update points_interet.py

--- Scripts/points_interet.py
+++ Scripts/points_interet.py
@@ -8,7 +8,7 @@ def points_interet(POI_EPSG, Isochrone10V):
   iso10v = iface.addVectorLayer("Isochrone10V", "isochrone10v", "ogr")
   
   poi.removeSelection()
-  processing.runalg('qgis:extractbylocation', poi, isochrone, u'within', 0, ".\POIiso.shp")
+  processing.runalg('qgis:extractbylocation', poi, iso10v, u'within', 0, ".\POIiso.shp")
   POIiso = iface.addVectorLayer(".\POIiso.shp", "POIiso", "ogr")
   
   nombre_POI = POIiso.featureCount()

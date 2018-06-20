1cb45126e6c821a983f6950423a534a7c49fb1e8
Chifoumi78 <39900864+Chifoumi78@users.noreply.github.com>
2018-06-20 13:52:21 +0200

    Indicateur POI
    Création de l'indicateur

---
+++ Scripts/Points_interet.py
@@ -0,0 +1,13 @@
+import processing
+
+def points_interet(POI_EPSG, Isochrone10V):
+  
+  poi = iface.addVectorLayer("POI_EPSG", "POI", "ogr")
+  iso10v = iface.addVectorLayer("Isochrone10V", "isochrone10v", "ogr")
+  
+  poi.removeSelection()
+  processing.runalg('qgis:extractbylocation', poi, isochrone, u'within', 0, ".\POIiso.shp")
+  POIiso = iface.addVectorLayer(".\POIiso.shp", "POIiso", "ogr")
+  
+  nombre_POI = POIiso.featureCount()
+  return nombre_POI

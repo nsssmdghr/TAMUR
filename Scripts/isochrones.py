26185665eb8c7ea8df6a93f1bdf41c9477773632
Chifoumi78 <39900864+Chifoumi78@users.noreply.github.com>
2018-06-21 08:57:07 +0200

    Create isochrones.py

---
+++ Scripts/isochrones.py
@@ -0,0 +1,11 @@
+from qgis.core import *
+from qgis.utils import iface 
+
+
+def isochrones():
+  
+  
+  
+  flag = 0
+    while flag != 1:
+      flag = raw_input("Veuillez suivre la section 'Isochrones' du mode d'emploi, puis taper 1 et valider avec Entree.")

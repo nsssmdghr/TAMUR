0845a584c7c02d07b275b06d309037b039447cb3
nsssmdghr <39874463+nsssmdghr@users.noreply.github.com>
2018-06-21 20:18:05 +0300

    Update points_interet.py
    ajout de la repartition et de la proximite des poi

--- Scripts/points_interet.py
+++ Scripts/points_interet.py
@@ -3,7 +3,7 @@ from qgis.utils import iface
 from qgis.analysis import QgsGeometryAnalyzer
 import processing
 
-def points_interet(POI_EPSG, Isochrone10V):
+def points_interet(POI_EPSG, Isochrone10V, param):
   
   poi = iface.addVectorLayer("POI_EPSG", "POI", "ogr")
   iso10v = iface.addVectorLayer("Isochrone10V", "isochrone10v", "ogr")
@@ -13,4 +13,16 @@ def points_interet(POI_EPSG, Isochrone10V):
   POIiso = iface.addVectorLayer(".\POIiso.shp", "POIiso", "ogr")
   
   nombre_POI = POIiso.featureCount()
-  return nombre_POI
+  
+  flag = 0
+  while flag not in [1,2,3]:
+    flag = raw_input('Veuillez entrer le numero correspondant a la repartition des points d\'interet, puis valider avec Entree : \n1:uniforme autour du quartier \n2:homogène autour du quartier \n3:concentrée d\'un seul côté du quartier \n')
+  rep = flag
+  
+  flag = 0
+  while flag not in [1,2,3]:
+    flag = raw_input('Veuillez entrer le numero correspondant a la proximite des points d\'interet, puis valider avec Entree : \n1:la plupart des points sont proches du quartier \n2: points à distances moyennes ou hétérogènes du quartier \n3:points éloignés du quartier \n')
+  pro = flag
+  
+  
+  return param['R{0}'.format(rep)] * param['PR{0}'.format(pro)] * nombre_POI

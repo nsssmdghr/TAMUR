8ce28c63fdb590a69695125713b91a60fcac5b27
nsssmdghr <39874463+nsssmdghr@users.noreply.github.com>
2018-06-22 04:29:44 +0300

    Update deciles.py
    ordre alphabétique des indicateurs

--- Scripts/deciles.py
+++ Scripts/deciles.py
@@ -1,11 +1,11 @@
 def get_note(valeur, nom_indic, tab_deciles):
-	if nom_indic == 'accessibilite vp':
+	if nom_indic == 'accessibilite tc':
 		row = 0
-	elif nom_indic == 'accessibilite tc':
+	elif nom_indic == 'accessibilite vp':
 		row = 1
-	elif nom_indic == 'points interet':
-		row = 2
 	elif nom_indic == 'couverture commerciale':
+		row = 2
+	elif nom_indic == 'points interet':
 		row = 3
 	elif nom_indic == 'potentiel commercial':
 		row = 4

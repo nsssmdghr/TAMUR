aca723d599a5755107d06caf58e5153c9511fd50
nsssmdghr <39874463+nsssmdghr@users.noreply.github.com>
2018-06-22 14:40:10 +0300

    Update agregation.py
    normalisation avant agregation

--- Scripts/agregation.py
+++ Scripts/agregation.py
@@ -1,8 +1,15 @@
-def agreg(notes, param, moyennes, corresp):
+def agregation(notes, moyennes):
+	out = {}
+	for id in notes:
+		out[id] = notes[id] / moyennes[id]
+	return out
+
+
+def agreg(notes, param, corresp):
 	notes_branches = {CP:0, PA:0, CU:0, CF:0, NA:0, CE:0, CM:0}
 	somme_pond = {CP:0, PA:0, CU:0, CF:0, NA:0, CE:0, CM:0}
 		for id_entree in notes:
-  			notes_branches[id_entree[:2]] += notes[id_entree] * param[corresp[id_entree]] / moyennes[id_entree]
+  			notes_branches[id_entree[:2]] += notes[id_entree] * param[corresp[id_entree]]
   			somme_pond[id_entree[:2]] += param[corresp[id_entree]]
 		for id in notes_branches:
  			notes_branches[id] /= somme_pond[id]

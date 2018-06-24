2bb90f482632407d20d80d813d85e26c1b5dda35
nsssmdghr <39874463+nsssmdghr@users.noreply.github.com>
2018-06-24 23:06:32 +0200

    Incorporation isochrones

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from Scripts.deciles import *
 from Scripts.points_interet import *
 from Scripts.agregation import *
 from Scripts.export import *
-from Scripts.isochrones import *
 from Scripts.couverture_commerciale import *
 
 #Definition du chemin vers le dossier principal, a modifier manuellement
@@ -43,7 +42,34 @@ notes = {}
 ind_brut = {}
 
 #Préparation des isochrones
-isochrones('COMMUNES_OSM.shp')
+geo = iface.addVectorLayer('COMMUNES_OSM.shp',"GEO","ogr")
+  
+flag = 0
+  while flag != 1:
+    flag = raw_input("Veuillez suivre le mode d'emploi pour créer une isochrone de 10 min à pieds, puis taper 1 et valider avec Entree.")
+
+iso10p = iface.activeLayer()
+QgsVectorFileWriter.writeAsVectorFormat(iso10p, "Isochrone10P.shp", "utf-8", iso10p.crs(), "ESRI Shapefile")
+QgsMapLayerRegistry.instance().removeMapLayers( [iso10p.id()] )
+
+flag = 0
+  while flag != 1:
+    flag = raw_input("Veuillez suivre le mode d'emploi pour créer une isochrone de 10 min en voiture, puis taper 1 et valider avec Entree.")
+
+iso10v = iface.activeLayer()
+QgsVectorFileWriter.writeAsVectorFormat(iso10p, "Isochrone10V.shp", "utf-8", iso10v.crs(), "ESRI Shapefile")
+QgsMapLayerRegistry.instance().removeMapLayers( [iso10v.id()] )
+
+flag = 0
+  while flag != 1:
+    flag = raw_input("Veuillez suivre le mode d'emploi pour créer une isochrone de 30 min en voiture, puis taper 1 et valider avec Entree.")
+
+iso30v = iface.activeLayer()
+QgsVectorFileWriter.writeAsVectorFormat(iso30v, "Isochrone30V.shp", "utf-8", iso30v.crs(), "ESRI Shapefile")
+QgsMapLayerRegistry.instance().removeMapLayers( [iso30v.id()] )
+
+print("Les isochrones ont bien été créées")
+
 
 #Traitement potentiel commercial
 pot_com = potentiel_commercial('IRIS.shp', 'COMMUNES.csv', 'REV_IRIS.csv', 'POP_IRIS.csv', num_iris, param['n'])
